3cffc594520d114adb094426ec69d3978c286a7b
uncenter <47499684+uncenter@users.noreply.github.com>
2024-10-18 15:06:22 -0400

    fix: make statements encapsulate content

--- grammar.js
+++ grammar.js
@@ -5,14 +5,7 @@ module.exports = grammar({
   name: "tera",
 
   rules: {
-    source_file: $ => repeat(
-      choice(
-        $.expression_tag,
-        $.statement_tag,
-        $.comment_tag,
-        $.content
-      )
-    ),
+    source_file: $ => repeat($._template),
 
     bool: $ => token(/[Tt]rue|[Ff]alse/),
     number: $ => token(/[\+-]?[0-9]+(\.[0-9]+)?/),
@@ -55,13 +48,24 @@ module.exports = grammar({
     expression_tag: $ => seq(
       "{{", optional("-"), $._expr, optional("-"), "}}"
     ),
-    begin_if_statement: $ => statement(seq("if", $._expr)),
-    begin_for_statement: $ => statement(seq(
-      "for",
-      $.identifier, optional(seq(",", $.identifier)),
-      "in",
-      $._expr,
-    )),
+
+    if_statement: $ => seq(
+      statement(seq("if", $._expr)),
+      choice(repeat($._template), statement("else"), statement(seq("elif", $._expr))),
+      statement("endif")
+    ),
+    for_statement: $ => seq(
+      statement(seq(
+        "for",
+        $.identifier, optional(seq(",", $.identifier)),
+        "in",
+        $._expr,
+      )),
+      choice(repeat($._template), statement("break"), statement("continue")),
+      statement(
+        "endfor"
+      )
+    ),
     set_statement: $ => statement(seq(
       choice("set", "set_global"), $.assignment_expression
     )),
@@ -74,30 +78,52 @@ module.exports = grammar({
     extends_statement: $ => statement(seq(
       "extends", $.string
     )),
-    begin_macro_statement: $ => statement(seq(
-     "macro", $.identifier,
-     "(",
-     repeat(seq($.identifier, optional(seq("=", $.literal)), optional(","))),
-      ")"
-    )),
-    generic_named_statement: $ => statement(seq(choice("filter", "block", "endblock", "endmacro"), $.identifier)),
-    generic_statement: $ => seq("{%", optional("-"), choice("elif", "else", "endif", "endfor", "endfilter", "break", "continue"), optional("-"), "%}"),
-    statement_tag: $ => choice(
-      $.begin_if_statement,
-      $.begin_for_statement,
+    macro_statement: $ => seq(
+      statement(seq(
+       "macro", $.identifier,
+       "(",
+       repeat(seq($.identifier, optional(seq("=", $.literal)), optional(","))),
+        ")"
+      )),
+      repeat($._template),
+      statement("endmacro")
+    ),
+    filter_statement: $ => seq(
+      statement(seq(
+        "filter", $.identifier
+      )),
+      repeat($._template),
+      statement("endfilter")
+    ),
+    block_statement: $ => seq(
+      statement(seq(
+        "block", $.identifier
+      )),
+      repeat($._template),
+      statement(seq("endblock", $.identifier))
+    ),
+    _statement_tag: $ => choice(
+      $.if_statement,
+      $.for_statement,
       $.set_statement,
       $.include_statement,
       $.import_statement,
       $.extends_statement,
-      $.begin_macro_statement,
-      $.generic_named_statement,
-      $.generic_statement,
+      $.macro_statement,
+      $.filter_statement,
+      $.block_statement
     ),
     comment_tag: $ => seq(
       "{#", optional("-"), /[^#]+/, optional("-"), "#}"
     ),
 
     content: $ => /([^{]|[{][^{%#])+/,
+    _template: $ => choice(
+      $._statement_tag,
+      $.expression_tag,
+      $.comment_tag,
+      $.content
+    ),
   }
 });
 
