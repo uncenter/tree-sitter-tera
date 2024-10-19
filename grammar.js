91bb91f46f9935a866db225aff4297be7377b3ae
uncenter <47499684+uncenter@users.noreply.github.com>
2024-10-19 00:17:21 -0400

    style: format grammar.js with prettier

--- grammar.js
+++ grammar.js
@@ -2,156 +2,214 @@
 // @ts-check
 
 module.exports = grammar({
-  name: "tera",
+	name: 'tera',
 
-  rules: {
-    source_file: $ => repeat($._template),
-    content: $ => /([^{]|[{][^{%#])+/,
+	rules: {
+		source_file: ($) => repeat($._template),
+		content: ($) => /([^{]|[{][^{%#])+/,
 
-    /* Primitives */
-    bool: $ => token(/[Tt]rue|[Ff]alse/),
-    number: $ => token(/[\+-]?[0-9]+(\.[0-9]+)?/),
-    string: $ => choice(
-        seq("'", repeat(/[^']/), "'"),
-        seq('"', repeat(/[^"]/), '"'),
-        seq('`', repeat(/[`"]/), '`')
-      ),
-    _literal: $ => choice($.bool, $.number, $.string),
-    array: $ => seq("[", repeat(seq($._value, optional(","))), "]"),
-    identifier: $ => /\w+/,
-    
-    /* Expressions */
-    member_expression: $ => seq($.identifier, repeat1(seq(".", alias($.identifier, $.property_identifier)))),
-    binary_expression: $ => {
-      const exp = (op) => seq(field('left', $._value), field('operator', op), field('right', $._value));
-      return choice(
-        ...['*', '/', '%', "|"].map(op => prec.left(2, exp(op))),
-        ...['+', '-', '~'].map(op => prec.left(1, exp(op))),
-        ...["==", "!=", "<", ">", "<=", ">=", 'in', 'and', 'or', 'is'].map(op => prec.left(exp(op)))
-      );
-    },
-    unary_expression: $ => prec(3, choice(
-      seq('-', $._value),
-      seq("not", $._value)
-    )),
-    assignment_expression: $ => seq(field('left', choice($.identifier, $.member_expression)), field('operator', "="), field('right', $._value)),
-    call_expression: $ => seq(
-      optional(seq(field('scope', $.identifier), "::")),
-      field('name', $.identifier),
-      "(", repeat(seq($.identifier, "=", $._value, optional(","))), ")"
-    ),
+		/* Primitives */
+		bool: ($) => token(/[Tt]rue|[Ff]alse/),
+		number: ($) => token(/[\+-]?[0-9]+(\.[0-9]+)?/),
+		string: ($) =>
+			choice(
+				seq("'", repeat(/[^']/), "'"),
+				seq('"', repeat(/[^"]/), '"'),
+				seq('`', repeat(/[`"]/), '`'),
+			),
+		_literal: ($) => choice($.bool, $.number, $.string),
+		array: ($) => seq('[', repeat(seq($._value, optional(','))), ']'),
+		identifier: ($) => /\w+/,
 
-    // Something that can resolve to a value.
-    _value: $ => seq(choice(
-      $.identifier,
-      $.member_expression,
+		/* Expressions */
+		member_expression: ($) =>
+			seq(
+				$.identifier,
+				repeat1(seq('.', alias($.identifier, $.property_identifier))),
+			),
+		binary_expression: ($) => {
+			const exp = (op) =>
+				seq(
+					field('left', $._value),
+					field('operator', op),
+					field('right', $._value),
+				);
+			return choice(
+				...['*', '/', '%', '|'].map((op) => prec.left(2, exp(op))),
+				...['+', '-', '~'].map((op) => prec.left(1, exp(op))),
+				...[
+					'==',
+					'!=',
+					'<',
+					'>',
+					'<=',
+					'>=',
+					'in',
+					'and',
+					'or',
+					'is',
+				].map((op) => prec.left(exp(op))),
+			);
+		},
+		unary_expression: ($) =>
+			prec(3, choice(seq('-', $._value), seq('not', $._value))),
+		assignment_expression: ($) =>
+			seq(
+				field('left', choice($.identifier, $.member_expression)),
+				field('operator', '='),
+				field('right', $._value),
+			),
+		call_expression: ($) =>
+			seq(
+				optional(seq(field('scope', $.identifier), '::')),
+				field('name', $.identifier),
+				'(',
+				repeat(seq($.identifier, '=', $._value, optional(','))),
+				')',
+			),
 
-      $._literal,
-      $.array,
-      $.binary_expression,
-      $.unary_expression,
-      $.call_expression
-    )),
+		// Something that can resolve to a value.
+		_value: ($) =>
+			seq(
+				choice(
+					$.identifier,
+					$.member_expression,
 
-    /* Tags */
-    _template: $ => choice(
-      $._statement_tag,
-      $.expression_tag,
-      $.comment_tag,
-      $.content
-    ),
+					$._literal,
+					$.array,
+					$.binary_expression,
+					$.unary_expression,
+					$.call_expression,
+				),
+			),
 
-    // Comment
-    comment_tag: $ => seq(
-      "{#", optional("-"), /[^#]+/, optional("-"), "#}"
-    ),
-  
-    // Expression
-    expression_tag: $ => seq(
-      "{{", optional("-"), $._value, optional("-"), "}}"
-    ),
+		/* Tags */
+		_template: ($) =>
+			choice(
+				$._statement_tag,
+				$.expression_tag,
+				$.comment_tag,
+				$.content,
+			),
 
-    // Statements
-    _statement_tag: $ => choice(
-      $.if_statement,
-      $.for_statement,
-      $.for_break_statement,
-      $.for_continue_statement,
-      $.set_statement,
-      $.include_statement,
-      $.import_statement,
-      $.extends_statement,
-      $.macro_statement,
-      $.filter_statement,
-      $.block_statement
-    ),
+		// Comment
+		comment_tag: ($) =>
+			seq('{#', optional('-'), /[^#]+/, optional('-'), '#}'),
 
-    if_statement: $ => seq(
-      statement(field('condition', seq("if", $._value))),
-      field('consequence', repeat($._template)),
-      optional(field('alternative', repeat($.elif_clause))),
-      optional(field('alternative', $.else_clause)),
-      statement("endif")
-    ),
-    elif_clause: $ => prec.right(1,seq(
-      statement(field('condition', seq("elif", $._value))),
-      field('consequence', repeat($._template)),
-    )),
-    else_clause: $ => prec.right(1, seq(
-      statement("else"),
-      field('consequence', repeat($._template)),
-    )),
-    for_statement: $ => seq(
-      statement(seq(
-        "for",
-        field('left', seq($.identifier, optional(seq(",", $.identifier)))),
-        "in",
-        field('right', $._value),
-      )),
-      field('body', repeat($._template)),
-      statement("endfor")
-    ),
-    for_break_statement: $ => statement("break"),
-    for_continue_statement: $ => statement("continue"),
-    set_statement: $ => statement(seq(
-      choice("set", "set_global"), $.assignment_expression
-    )),
-    include_statement: $ => statement(seq(
-     "include", choice($.string, $.array), optional("ignore missing")
-    )),
-    import_statement: $ => statement(seq(
-      "import", $.string, "as", field('scope', $.identifier)
-    )),
-    extends_statement: $ => statement(seq(
-      "extends", $.string
-    )),
-    macro_statement: $ => seq(
-      statement(seq(
-       "macro", field('name', $.identifier),
-       "(",
-       repeat(seq(field('parameter', $.identifier), optional(seq("=", $._literal)), optional(","))),
-        ")"
-      )),
-      repeat($._template),
-      statement(seq("endmacro", $.identifier))
-    ),
-    filter_statement: $ => seq(
-      statement(seq(
-        "filter", $.identifier
-      )),
-      repeat($._template),
-      statement("endfilter")
-    ),
-    block_statement: $ => seq(
-      statement(seq(
-        "block", $.identifier
-      )),
-      repeat($._template),
-      statement(seq("endblock", $.identifier))
-    ),
-  }
+		// Expression
+		expression_tag: ($) =>
+			seq('{{', optional('-'), $._value, optional('-'), '}}'),
+
+		// Statements
+		_statement_tag: ($) =>
+			choice(
+				$.if_statement,
+				$.for_statement,
+				$.for_break_statement,
+				$.for_continue_statement,
+				$.set_statement,
+				$.include_statement,
+				$.import_statement,
+				$.extends_statement,
+				$.macro_statement,
+				$.filter_statement,
+				$.block_statement,
+			),
+
+		if_statement: ($) =>
+			seq(
+				statement(field('condition', seq('if', $._value))),
+				field('consequence', repeat($._template)),
+				optional(field('alternative', repeat($.elif_clause))),
+				optional(field('alternative', $.else_clause)),
+				statement('endif'),
+			),
+		elif_clause: ($) =>
+			prec.right(
+				1,
+				seq(
+					statement(field('condition', seq('elif', $._value))),
+					field('consequence', repeat($._template)),
+				),
+			),
+		else_clause: ($) =>
+			prec.right(
+				1,
+				seq(
+					statement('else'),
+					field('consequence', repeat($._template)),
+				),
+			),
+		for_statement: ($) =>
+			seq(
+				statement(
+					seq(
+						'for',
+						field(
+							'left',
+							seq($.identifier, optional(seq(',', $.identifier))),
+						),
+						'in',
+						field('right', $._value),
+					),
+				),
+				field('body', repeat($._template)),
+				statement('endfor'),
+			),
+		for_break_statement: ($) => statement('break'),
+		for_continue_statement: ($) => statement('continue'),
+		set_statement: ($) =>
+			statement(
+				seq(choice('set', 'set_global'), $.assignment_expression),
+			),
+		include_statement: ($) =>
+			statement(
+				seq(
+					'include',
+					choice($.string, $.array),
+					optional('ignore missing'),
+				),
+			),
+		import_statement: ($) =>
+			statement(
+				seq('import', $.string, 'as', field('scope', $.identifier)),
+			),
+		extends_statement: ($) => statement(seq('extends', $.string)),
+		macro_statement: ($) =>
+			seq(
+				statement(
+					seq(
+						'macro',
+						field('name', $.identifier),
+						'(',
+						repeat(
+							seq(
+								field('parameter', $.identifier),
+								optional(seq('=', $._literal)),
+								optional(','),
+							),
+						),
+						')',
+					),
+				),
+				repeat($._template),
+				statement(seq('endmacro', $.identifier)),
+			),
+		filter_statement: ($) =>
+			seq(
+				statement(seq('filter', $.identifier)),
+				repeat($._template),
+				statement('endfilter'),
+			),
+		block_statement: ($) =>
+			seq(
+				statement(seq('block', $.identifier)),
+				repeat($._template),
+				statement(seq('endblock', $.identifier)),
+			),
+	},
 });
 
 function statement(inner) {
-  return seq("{%", optional("-"), inner, optional("-"), "%}");
+	return seq('{%', optional('-'), inner, optional('-'), '%}');
 }
