8313684f873965e6de787d7a9cd0f6f38e87af13
uncenter <47499684+uncenter@users.noreply.github.com>
2024-10-20 01:56:44 -0400

    feat(queries/highlights): highlight builtin variables, functions, filters, and tests
    Closes #5.

--- queries/highlights.scm
+++ queries/highlights.scm
@@ -5,6 +5,11 @@
 
 (identifier) @variable
 
+((identifier) @variable.builtin
+  (#any-of? @variable.builtin
+    "loop"
+    "__tera_context"))
+
 ; Properties
 ;-----------
 
@@ -120,8 +125,85 @@
   scope: (identifier)? @namespace
   name: (identifier) @function)
 
+(call_expression
+  name: (identifier) @function.builtin
+  (#any-of? @function.builtin
+    ; Functions - https://keats.github.io/tera/docs/#built-in-functions
+    "range"
+    "now"
+    "throw"
+    "get_random"
+    "get_env"
+    ; Tests - https://keats.github.io/tera/docs/#built-in-tests
+    "defined"
+    "undefined"
+    "odd"
+    "even"
+    "string"
+    "number"
+    "divisibleby"
+    "iterable"
+    "object"
+    "starting_with"
+    "ending_with"
+    "containing"
+    "matching"))
+
 (filter_expression
   filter: (identifier) @function.method)
+
+(filter_expression
+  filter: (identifier) @function.builtin
+  (#any-of? @function.builtin
+    ; Filters - https://keats.github.io/tera/docs/#built-in-filters
+    "lower"
+    "upper"
+    "wordcount"
+    "capitalize"
+    "replace"
+    "addslashes"
+    "slugify"
+    "title"
+    "trim"
+    "trim_start"
+    "trim_end"
+    "trim_start_matches"
+    "trim_end_matches"
+    "truncate"
+    "linebreaksbr"
+    "spaceless"
+    "indent"
+    "striptags"
+    "first"
+    "last"
+    "nth"
+    "join"
+    "length"
+    "reverse"
+    "sort"
+    "unique"
+    "slice"
+    "group_by"
+    "filter"
+    "map"
+    "concat"
+    "urlencode"
+    "urlencode_strict"
+    "abs"
+    "pluralize"
+    "round"
+    "filesizeformat"
+    "date"
+    "escape"
+    "escape_xml"
+    "safe"
+    "get"
+    "split"
+    "int"
+    "float"
+    "json_encode"
+    "as_str"
+    "default"))
 
 ; Namespaces
 
