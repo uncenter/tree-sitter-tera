0c01bee2009ce7b6976b3c34e888feb4124c5502
uncenter <47499684+uncenter@users.noreply.github.com>
2024-11-24 15:23:18 -0500

    feat(queries): add more whiskers-specific

--- helix-queries/injections.scm
+++ helix-queries/injections.scm
@@ -1,5 +1,33 @@
 (
   (frontmatter_content) @yaml_frontmatter
+    (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.css\"\\s*$")
+  (content) @injection.content
+  (#set! injection.language "css")
+  (#set! injection.combined)
+)
+(
+  (frontmatter_content) @yaml_frontmatter
+    (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.html\"\\s*$")
+  (content) @injection.content
+  (#set! injection.language "html")
+  (#set! injection.combined)
+)
+(
+  (frontmatter_content) @yaml_frontmatter
+    (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.json\"\\s*$")
+  (content) @injection.content
+  (#set! injection.language "json")
+  (#set! injection.combined)
+)
+(
+  (frontmatter_content) @yaml_frontmatter
+    (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.yaml\"\\s*$")
+  (content) @injection.content
+  (#set! injection.language "yaml")
+  (#set! injection.combined)
+)
+(
+  (frontmatter_content) @yaml_frontmatter
     (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.toml\"\\s*$")
   (content) @injection.content
   (#set! injection.language "toml")
--- queries/injections.scm
+++ queries/injections.scm
@@ -1,9 +1,39 @@
 ((frontmatter_content) @injection.content
   (#set! injection.language "yaml"))
+
+; catppuccin/whiskers-specific
 (
   (frontmatter_content) @yaml_frontmatter
     (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.toml\"\\s*$")
   (content) @injection.content
   (#set! injection.language "toml")
+  (#set! injection.combined)
+)
+(
+  (frontmatter_content) @yaml_frontmatter
+    (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.yaml\"\\s*$")
+  (content) @injection.content
+  (#set! injection.language "yaml")
+  (#set! injection.combined)
+)
+(
+  (frontmatter_content) @yaml_frontmatter
+    (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.json\"\\s*$")
+  (content) @injection.content
+  (#set! injection.language "json")
+  (#set! injection.combined)
+)
+(
+  (frontmatter_content) @yaml_frontmatter
+    (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.html\"\\s*$")
+  (content) @injection.content
+  (#set! injection.language "html")
+  (#set! injection.combined)
+)
+(
+  (frontmatter_content) @yaml_frontmatter
+    (#match? @yaml_frontmatter "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*\".+\\.css\"\\s*$")
+  (content) @injection.content
+  (#set! injection.language "css")
   (#set! injection.combined)
 )
