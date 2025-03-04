9d70650e64e99c6818940ab488385347d2037014
uncenter <uncenter@uncenter.dev>
2025-03-04 11:40:07 -0500

    chore!: remove whiskers-specific injections
    These regex patterns already don't work in Neovim (some pattern error) that is too much for me to deal with, and I'm hoping to come at this with a better solution not specific to Whiskers. See #2 for more.

--- helix-queries/injections.scm
+++ helix-queries/injections.scm
@@ -1,38 +1,3 @@
-(
-  (frontmatter) @fm
-    (#match? @fm "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*.+\\.css")
-  (content) @injection.content
-  (#set! injection.language "css")
-  (#set! injection.combined)
-)
-(
-  (frontmatter) @fm
-    (#match? @fm "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*.+\\.html")
-  (content) @injection.content
-  (#set! injection.language "html")
-  (#set! injection.combined)
-)
-(
-  (frontmatter) @fm
-    (#match? @fm "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*.+\\.json")
-  (content) @injection.content
-  (#set! injection.language "json")
-  (#set! injection.combined)
-)
-(
-  (frontmatter) @fm
-    (#match? @fm "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*.+\\.yaml")
-  (content) @injection.content
-  (#set! injection.language "yaml")
-  (#set! injection.combined)
-)
-(
-  (frontmatter) @fm
-    (#match? @fm "whiskers:\\s*\\n(?:\\s{2,}\\S.*\\n)*\\s{2,}filename:\\s*.+\\.toml")
-  (content) @injection.content
-  (#set! injection.language "toml")
-  (#set! injection.combined)
-)
 (frontmatter (content) @injection.content
   (#set! injection.language "yaml")
   (#set! injection.combined)
